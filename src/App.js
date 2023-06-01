d0873fdcb2cde2344d2fb69f040d1fb1a296c780
AbdurRehmanMuhammad <111024434+AbdurRehmanMuhammad@users.noreply.github.com>
2023-06-01 15:56:19 +0500

    second

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Cloned and updated
+          Edit <code>src/App.js</code> Cloned and updated second
         </p>
         <a
           className="App-link"
